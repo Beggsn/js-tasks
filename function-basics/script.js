344c01608900c5d40622c033403b4a02b6b29514
Berkant Cetin <b.cetin.berlin@icloud.com>
2023-07-21 11:13:09 +0200

    refactor: last function with Math.abs()

--- function-basics/script.js
+++ function-basics/script.js
@@ -70,14 +70,18 @@ console.log(addPositive(-1, -8));
 // result3 should be 9
 */
 
+// function addPositive(num1, num2) {
+//     if (num1 < 0) {
+//         num1 = num1 * -1;
+//     }
+//     if (num2 < 0) {
+//         num2 = num2 * -1;
+//     }
+//     return num1 + num2;
+// }
 function addPositive(num1, num2) {
-    if (num1 < 0) {
-        num1 = num1 * -1;
-    }
-    if (num2 < 0) {
-        num2 = num2 * -1;
-    }
-    return num1 + num2;
+    
+    return Math.abs(num1 + num2);
 }
 
 console.log(addPositive(2, 3));

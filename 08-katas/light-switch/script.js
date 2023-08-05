df5a70cb82acc3d6f792341432329cf344f7e6f1
Berkant Cetin <b.cetin.berlin@icloud.com>
2023-08-05 11:12:45 +0200

    feat: Adds dark mode functionality to button

---
+++ 08-katas/light-switch/script.js
@@ -0,0 +1,23 @@
+// function logEvent(event) {
+//     console.log(event.target);
+//     console.log(event.currentTarget);
+//     console.log(event.eventPhase);
+// }
+
+let dark = false;
+const btn = document.querySelector('.button');
+btn.addEventListener('click', function() {
+  dark = !dark;
+//   console.log(dark);
+  if (dark) {
+    document.body.classList.add('black');
+    document.body.classList.remove('white');
+    btn.classList.add('button-black');
+    document.title = 'Good Night';
+  } else {
+    document.body.classList.remove('black');
+    document.body.classList.add('white');
+    btn.classList.remove('button-black');
+    document.title = 'Good Morning';
+  }
+});

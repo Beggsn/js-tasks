b0472d203f268ed0d99c01a904cdbe563f117b3b
Berkant Cetin <b.cetin.berlin@icloud.com>
2023-08-07 14:53:48 +0200

    feat: implements a toggle for three checkboxes

--- 09-coding-tasks/fast-cheap-good/script.js
+++ 09-coding-tasks/fast-cheap-good/script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const checkboxes = document.querySelectorAll('input[type="checkbox"]'); 
+const maxChecked = 2; 
+let checkedBoxes = 0;
+let lastChecked;
+
+checkboxes.forEach(function (box) {
+  box.addEventListener("change", function () {
+    if (box.checked === true) {
+      checkedBoxes++;
+    } else {
+      checkedBoxes--;
+    }
+    if (checkedBoxes > maxChecked) {
+      lastChecked.checked = false;
+    }
+    lastChecked = box;
+  });
+});

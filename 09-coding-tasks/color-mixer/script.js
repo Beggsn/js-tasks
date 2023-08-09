e9b418f0483eb1071da6fd31e58a48ae799a2cb3
Berkant Cetin <b.cetin.berlin@icloud.com>
2023-08-09 12:24:08 +0200

    refactor: updateColor function & commenting code

--- 09-coding-tasks/color-mixer/script.js
+++ 09-coding-tasks/color-mixer/script.js
@@ -1,35 +1,55 @@
 "use strict";
-// get slider element
+
+// Slider-Elemente abrufen
 const redSlider = document.querySelector("#red");
 const greenSlider = document.querySelector("#green");
 const blueSlider = document.querySelector("#blue");
+
+// Elemente abrufen
 const header = document.querySelector(".header");
 const hexCode = document.querySelector(".hex");
 const main = document.querySelector("main");
 
-// get eventListeners to the sliders
+// Event-Listener zu den Slidern hinzufügen
 redSlider.addEventListener("input", updateColor);
 greenSlider.addEventListener("input", updateColor);
 blueSlider.addEventListener("input", updateColor);
 
+// Funktion zum Aktualisieren der Farbe
 function updateColor() {
   const redValue = redSlider.value;
   const greenValue = greenSlider.value;
   const blueValue = blueSlider.value;
-  hexCode.textContent = `#${decimalToHex(redValue)}${decimalToHex(greenValue)}${decimalToHex(blueValue)}`;
-  header.style.backgroundColor = `rgb(${redValue}, ${greenValue}, ${blueValue}, 0.25)`;
-  main.style.backgroundColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+
+  const hexValue = `#${decimalToHex(redValue)}${decimalToHex(greenValue)}${decimalToHex(blueValue)}`;
+  const rgbValue = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+
+  hexCode.textContent = hexValue;
+  header.style.backgroundColor = `rgba(${redValue}, ${greenValue}, ${blueValue}, 0.25)`;
+  main.style.backgroundColor = rgbValue;
 }
 
+// Farbe initial aktualisieren
 updateColor();
 
+// Hilfsfunktion zur Konvertierung von Dezimal in Hexadezimal
 function decimalToHex(value) {
   return value.toString(16);
 }
 
-/*
-function decimalToHex(value) {
-  let hexStr = Number(value).toString(16);
-  return hexStr;
-}
-*/ 
+
+// old version
+// function updateColor() {
+//   const redValue = redSlider.value;
+//   const greenValue = greenSlider.value;
+//   const blueValue = blueSlider.value;
+//   hexCode.textContent = `#${decimalToHex(redValue)}${decimalToHex(greenValue)}${decimalToHex(blueValue)}`;
+//   header.style.backgroundColor = `rgb(${redValue}, ${greenValue}, ${blueValue}, 0.25)`;
+//   main.style.backgroundColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+// }
+
+// function decimalToHex(value) {
+//   let hexStr = Number(value).toString(16);
+//   return hexStr;
+// }
+

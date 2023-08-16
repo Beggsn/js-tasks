54203f22c694c86c3d0a2c05cf4b23844a6e3f67
Berkant Cetin <b.cetin.berlin@icloud.com>
2023-08-16 17:40:42 +0200

    "chore: add code comments"

--- 09-coding-tasks/quote/script.js
+++ 09-coding-tasks/quote/script.js
@@ -1,9 +1,11 @@
 "use strict";
 
+// DOM Elemente selektieren
 const btn = document.querySelector("button");
 const quoteText = document.querySelector(".quote-text");
 const quoteAuthor = document.querySelector(".quote-author");
 
+// Überprüfung, ob Quote-Text und Autor im localStorage vorhanden sind
 if (localStorage.getItem("quote")) {
   quoteText.textContent = localStorage.getItem("quote");
 }
@@ -15,13 +17,17 @@ btn.addEventListener("click", function () {
   fetch("https://dummy-apis.netlify.app/api/quote")
     .then((response) => response.json())
     .then((data) => {
+      // console.log(data);
+      // Aktualisieren des Textes und des Autors mit abgerufenen Werten
       quoteText.textContent = data.quote;
       quoteAuthor.textContent = `- ${data.author}`;
 
+      // Speichern der Daten im localStorage
       localStorage.setItem("quote", data.quote);
       localStorage.setItem("author", data.author);
 
     })
+    // Fehlerbehandlung 
     .catch((error) => {
       console.log(error);
     });

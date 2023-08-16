f9917370f164869549035f2ace48296844c3fcbf
Berkant Cetin <b.cetin.berlin@icloud.com>
2023-08-16 17:35:22 +0200

    feat: adds localStorage functionality

--- 09-coding-tasks/quote/script.js
+++ 09-coding-tasks/quote/script.js
@@ -4,13 +4,23 @@ const btn = document.querySelector("button");
 const quoteText = document.querySelector(".quote-text");
 const quoteAuthor = document.querySelector(".quote-author");
 
+if (localStorage.getItem("quote")) {
+  quoteText.textContent = localStorage.getItem("quote");
+}
+if (localStorage.getItem("author")) {
+  quoteAuthor.textContent = `- ${localStorage.getItem("author")}`;
+}
+
 btn.addEventListener("click", function () {
   fetch("https://dummy-apis.netlify.app/api/quote")
     .then((response) => response.json())
     .then((data) => {
-    //   console.log(data);
       quoteText.textContent = data.quote;
       quoteAuthor.textContent = `- ${data.author}`;
+
+      localStorage.setItem("quote", data.quote);
+      localStorage.setItem("author", data.author);
+
     })
     .catch((error) => {
       console.log(error);

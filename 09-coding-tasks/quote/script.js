4d8412c6d3699efa1952f8d2fb3bbee9e6e5d46e
Berkant Cetin <b.cetin.berlin@icloud.com>
2023-08-16 17:10:48 +0200

    feat: adds button click event to fetch "QUOTE API"

--- 09-coding-tasks/quote/script.js
+++ 09-coding-tasks/quote/script.js
@@ -1,2 +1,18 @@
 "use strict";
 
+const btn = document.querySelector("button");
+const quoteText = document.querySelector(".quote-text");
+const quoteAuthor = document.querySelector(".quote-author");
+
+btn.addEventListener("click", function () {
+  fetch("https://dummy-apis.netlify.app/api/quote")
+    .then((response) => response.json())
+    .then((data) => {
+    //   console.log(data);
+      quoteText.textContent = data.quote;
+      quoteAuthor.textContent = `- ${data.author}`;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});

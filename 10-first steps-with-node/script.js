5248c15b98d253a237bedd2e649767fdc46ebaf4
Berkant Cetin <b.cetin.berlin@icloud.com>
2023-08-21 22:43:47 +0200

    vendor: adds local dependencies & crud requests

--- 10-first steps-with-node/script.js
+++
@@ -1,7 +0,0 @@
-// // Import the fs module (File System) that comes with Node
-// const fs = require('fs');
-
-// // Write the string 'hello from node' to a file
-// fs.writeFileSync('c:\\Users\\beggo\\Desktop\\hello2.txt', 'hello from node');
-
-// console.log('Finished writing to file.');

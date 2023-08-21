18fe0dad909c66614d328c37aed16877f26ed3d2
Berkant Cetin <b.cetin.berlin@icloud.com>
2023-08-21 16:18:27 +0200

    initial commit

---
+++ 10-first steps-with-node/script.js
@@ -0,0 +1,7 @@
+// // Import the fs module (File System) that comes with Node
+// const fs = require('fs');
+
+// // Write the string 'hello from node' to a file
+// fs.writeFileSync('c:\\Users\\beggo\\Desktop\\hello2.txt', 'hello from node');
+
+// console.log('Finished writing to file.');

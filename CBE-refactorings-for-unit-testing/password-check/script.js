e61c29f50232344ae08e93f4cdb789a1c074fcf3
Berkant Cetin <b.cetin.berlin@icloud.com>
2023-08-29 11:40:58 +0200

    refactor: modularization and prep. for unit tests

--- CBE-refactorings-for-unit-testing/password-check/script.js
+++ CBE-refactorings-for-unit-testing/password-check/script.js
@@ -1,15 +1,3 @@
-const pwInput1 = document.querySelector('#password1');
-const pwInput2 = document.querySelector('#password2');
-const btn = document.querySelector('#button');
-const check = document.querySelector('#check');
+import { initApp } from "./lib.js";
 
-
-function checkPasswords() {
-    if (pwInput1.value === pwInput2.value) {
-        check.innerText = '✅';
-    } else {
-        check.innerText = '❌';
-    }
-}
-
-btn.addEventListener('click', checkPasswords);
+initApp();
